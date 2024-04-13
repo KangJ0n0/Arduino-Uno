2127a8f1d84ff547347df0ea0dd1efe9213e60ac
Afiftha Ravi Aufa <127705274+KangJ0n0@users.noreply.github.com>
2024-04-13 18:06:46 +0700

    test

--- Text/Text_animation.ino
+++ Text/Text_animation.ino
@@ -4,27 +4,23 @@ const int col = 20;
 const int row =  4;
 
 int position = 0;
-
-
 LiquidCrystal_I2C lcd(0x27, col, row);
+
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
 }
   
-
-}
-
 void loop() {
   lcd.clear();
   position += 1;
   lcd.setCursor(position, 0);
   lcd.print("now playing");
-  lcd.setCursor(position, 1)
-  lcd.print("少女レイ")
-  cd.setCursor(position, 3)
-  lcd.print("MORE MORE JUMP !")
+  lcd.setCursor(position, 1);
+  lcd.print("少女レイ");
+  cd.setCursor(position, 3);
+  lcd.print("MORE MORE JUMP !");
  delay(250);
 
  if(position < 0 || position >= col) {
