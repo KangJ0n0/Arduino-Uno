df8a704cfed4013b7d7cdd35d55c59a481a4e121
Afiftha Ravi Aufa <127705274+KangJ0n0@users.noreply.github.com>
2024-04-13 12:11:30 +0700

    Add files via upload

---
+++ Text_animation.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal_I2C.h>
+
+const int col = 20;
+const int row =  4;
+
+int position = 0;
+
+
+LiquidCrystal_I2C lcd(0x27, col, row);
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+  
+
+}
+
+void loop() {
+  lcd.clear();
+  position += 1;
+  lcd.setCursor(position, 0);
+  lcd.print("now playing");
+  lcd.setCursor(position, 1)
+  lcd.print("少女レイ")
+  cd.setCursor(position, 3)
+  lcd.print("MORE MORE JUMP !")
+ delay(250);
+
+ if(position < 0 || position >= col) {
+  position = 0;
+ }
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+}
