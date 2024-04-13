ac94a93d0839f539b512894475387aa15930691a
Afiftha Ravi Aufa <127705274+KangJ0n0@users.noreply.github.com>
2024-04-14 00:15:08 +0700

    bismillah bisa bikin wave bar

--- Text/Text_animation.ino
+++ Text/Text_animation.ino
@@ -1,29 +1,41 @@
 #include <LiquidCrystal_I2C.h>
 
-const int col = 20;
-const int row =  4;
-
-int position = 0;
-LiquidCrystal_I2C lcd(0x27, col, row);
-
+int screenWidth = 20;
+int screenHeight = 4;
+uint8_t speaker[] = {0x01, 0x03, 0x07, 0x1f, 0x1f, 0x07, 0x03, 0x01};
+String line1 = "now playing ";
+String line2 = "Seven (feat. Latto) - Clean Ver.";
+String line3 = "Jung Kook";
+int stringStart, stringEnd = 0;
+int scrollCursor = screenWidth;
+LiquidCrystal_I2C lcd(0x27, screenWidth, screenHeight);
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
+  lcd.begin(screenWidth, screenHeight);
+  lcd.createChar(0, speaker);
 }
-  
 void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  lcd.write(byte(0));
+  lcd.setCursor(0, 2);
+  lcd.print(line3);
+  lcd.setCursor(scrollCursor, 1);
+  lcd.print(line2.substring(stringStart,stringEnd));
+  delay(500);
   lcd.clear();
-  position += 1;
-  lcd.setCursor(position, 0);
-  lcd.print("now playing");
-  lcd.setCursor(position, 1);
-  lcd.print("少女レイ");
-  lcd.setCursor(position, 3);
-  lcd.print("MORE MORE JUMP !");
- delay(250);
-
- if(position < 0 || position >= col) {
-  position = 0;
- }
+  if(stringStart == 0 && scrollCursor > 0){
+    scrollCursor--;
+    stringEnd++;
+  }
+  else if (stringStart == stringEnd){
+    stringStart = stringEnd = 0;
+    scrollCursor = screenWidth;
+  } 
+  else if (stringEnd == line1.length() && scrollCursor == 0) {
+    stringStart++;
+  } 
+  else {
+    stringStart++;
+    stringEnd++;
+  }
 }
